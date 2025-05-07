841de3d2e6c7f58b0c7e34205b9756ef746989f7
Pammy@@ <pammytv.42@gmail.com>
2025-05-07 16:20:05 +0700

    login to Register

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoleBasedProductAPI.Controllers
+{
+    public class ProductsController1 : Controller
+    {
+        public IActionResult Products()
+        {
+            return View("views/Products/Products.cshtml");
+        }
+    }
+}
